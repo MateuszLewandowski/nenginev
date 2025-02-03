a3d96ce12ffc98fd1f6e29c5abbcfc53ebb396ad
Mateusz Lewandowski <mlewandowski@clickmeeting.com>
2025-02-03 18:43:57 +0100

    feat(tensor): describe math topics

--- src/Math/Tensor/Tensor.php
+++ src/Math/Tensor/Tensor.php
@@ -4,10 +4,26 @@ declare(strict_types=1);
 
 namespace App\Math\Tensor;
 
+use App\Math\Operation\Algebraic;
+use App\Math\Operation\Arithmetical;
+use App\Math\Operation\Clipable;
+use App\Math\Operation\Comparable;
+use App\Math\Operation\Reducible;
+use App\Math\Operation\Statistic;
+use App\Math\Operation\Trigonometrical;
+use App\Math\Values;
 use JsonSerializable;
 use Symfony\Component\Uid\Uuid;
 
-abstract readonly class Tensor implements JsonSerializable
+abstract readonly class Tensor implements
+    JsonSerializable
+//    Algebraic,
+//    Arithmetical,
+//    Clipable,
+//    Comparable,
+//    Reducible,
+//    Statistic,
+//    Trigonometrical
 {
     private Uuid $uid;
 
@@ -19,8 +35,10 @@ abstract readonly class Tensor implements JsonSerializable
 
     abstract public static function create(float|array $input): self;
     abstract public function isCompatible(self $tensor): bool;
-    abstract public function primitive(): mixed;
     abstract public function size(): int;
+    abstract public function values(): Values;
+    abstract public function primitive(): mixed;
+    abstract public function dimension(): int;
 
     public function id(): Uuid
     {
@@ -35,5 +53,15 @@ abstract readonly class Tensor implements JsonSerializable
     public function isScalar(): bool
     {
         return $this->type->isScalar();
+    }
+
+    public function isVector(): bool
+    {
+        return $this->type->isVector();
+    }
+
+    public function isMatrix(): bool
+    {
+        return $this->type->isMatrix();
     }
 }
