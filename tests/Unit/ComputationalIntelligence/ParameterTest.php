68cd6ee8053ae6bae2170162444426bf9a0fc4a6
Mateusz Lewandowski <mlewandowski@clickmeeting.com>
2025-02-08 11:44:11 +0100

    test(regression): impl parameter units

---
+++ tests/Unit/ComputationalIntelligence/ParameterTest.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Unit\ComputationalIntelligence;
+
+use App\ComputationalIntelligence\Model\Exception\NegativeValueException;
+use App\ComputationalIntelligence\Model\Parameter;
+use Generator;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(Parameter::class)]
+final class ParameterTest extends TestCase
+{
+    #[DataProvider('parameterValuesProvider')]
+    public function testCreateParameter(float|string|int $value): void
+    {
+        $parameter = new Parameter($value);
+
+        $this->assertSame((float) $value, $parameter->value);
+        $this->assertSame((int) $value, $parameter->asInteger());
+    }
+
+    public static function parameterValuesProvider(): Generator
+    {
+        yield [.0];
+        yield ['100'];
+        yield [1203];
+        yield [0x221];
+        yield ['123.445'];
+        yield [INF];
+    }
+
+    public function testTryToCreateParameterWithNegativeValue(): void
+    {
+        $this->expectException(NegativeValueException::class);
+
+        new Parameter('-.1');
+    }
+}
